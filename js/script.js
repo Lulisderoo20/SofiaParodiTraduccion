2298fd109791507e2f8e79f462628ffa1f2fb38e
Lucia Lisdero <lulisdero20@gmail.com>
2023-05-28 20:17:26 -0300

    config

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const nommbre= "Sofi";
+
+
+const navBtn = document.getElementById('nav-btn')
+const navLinks = document.getElementById('nav-links')
+const toTop = document.getElementById('to-top')
+const navLogo = document.getElementById('nav-logo')
+
+navBtn.addEventListener('click', () => {
+    navLinks.classList.toggle('hidden-links')
+})
+
+navLinks.addEventListener('click', () => {
+    navLinks.classList.toggle('hidden-links')
+})
+
+toTop.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
+})
+
+navLogo.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
+})
